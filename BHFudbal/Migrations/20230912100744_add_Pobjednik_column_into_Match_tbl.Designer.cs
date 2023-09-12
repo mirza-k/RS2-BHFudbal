0badbe113debb5ed997711831d2eb3469dbfddff
Mirza Kozica <mirza.kozica@edu.fit.ba>
2023-09-12 12:11:30 +0200

    Add Pobjednik column into Match table.

--- BHFudbal/Migrations/20230912100744_add_Pobjednik_column_into_Match_tbl.Designer.cs
+++ BHFudbal/Migrations/20230912100744_add_Pobjednik_column_into_Match_tbl.Designer.cs
@@ -4,14 +4,16 @@ using BHFudbal.BHFudbalDatabase;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace BHFudbal.Migrations
 {
     [DbContext(typeof(BHFudbalDBContext))]
-    partial class BHFudbalDBContextModelSnapshot : ModelSnapshot
+    [Migration("20230912100744_add_Pobjednik_column_into_Match_tbl")]
+    partial class add_Pobjednik_column_into_Match_tbl
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ BHFudbal/Migrations/20230912100744_add_Pobjednik_column_into_Match_tbl.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BHFudbal.Migrations
+{
+    public partial class add_Pobjednik_column_into_Match_tbl : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Pobjednik",
+                table: "Match",
+                type: "int",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Pobjednik",
+                table: "Match");
+        }
+    }
+}
